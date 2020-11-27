5dc457b57458e0797d6cbf201ecc21a739ee40a6
AdrianVazquezReyes <adrianvazquez3824@gmail.com>
2020-11-27 17:16:55 -0600

    new version, more complete, in the signin branch

--- hungry.py
+++ hungry.py
@@ -1,9 +1,12 @@
 hungry= input("are you hungry?")
+
 if hungry=="yes":
     print('eat something before work')
     print('eat pizza')
     print('eat burguer')
     print('from github')
-else:
+elif hungry=="not":
     print('get back to work')
     print('hello from github')
+else:
+    print('you can only choose yes or not')
